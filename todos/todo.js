a96cc20bf565e111e6a0bfed92acf7bc0fbcf646
ckaynatividad <natividad.ckay@gmail.com>
2021-10-28 14:51:46 -0700

    render function

--- todos/todo.js
+++ todos/todo.js
@@ -1,9 +1,35 @@
 import { toDo } from './render.js';
-import { setUser, generateUser, getUser } from '../utils';
+import { setUser, generateUser, getUser } from '../utils.js';
 
 function loadUser(){
-
+    
+    //const toDo 
     // getUser from localStorage
     const user = getUser();
     // Update DOM with user info 
+    const userName = document.getElementById('username');
+    const passWord = document.getElementById('password');
 }
+
+export function renderToDo(listItem){
+    const listItems = document.createElement('div');
+
+    const liItem = document.createElement('li');
+    listItem.textContent = toDo.todos;
+
+   
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = 'Completed?';
+    completeBtn.classList.add('finished');
+
+    renderToDo.append(listItems, liItem, completeBtn);
+    return renderToDo;
+} 
+console.log(toDo.todos[0]);
+
+const todoList = document.getElementById('todo-list');
+
+// for (let item of ){
+//     const listItems = renderToDo(item);
+//     todoList.append(listItems);
+// }
